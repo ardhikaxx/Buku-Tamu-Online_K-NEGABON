2faeff9c68720a77a2136ed7b4376e440c69d489
ardhikaxx <ardhikayanuar58@gmail.com>
2024-01-24 14:23:38 +0700

    update

--- js/script.js
+++ js/script.js
@@ -5,3 +5,23 @@ function showForm2() {
     form1.style.display = 'none';
     form2.style.display = 'block';
 }
+
+const buttons = document.querySelectorAll('.btn-mulai');
+
+buttons.forEach(button => {
+    button.addEventListener('click', function () {
+        const form2 = document.querySelector('.form-2');
+
+        const paragraph = form2.querySelector('.paragraf2').textContent;
+
+        Swal.fire({
+            icon: 'info',
+            iconColor: '#3C7AC0',
+            title: 'Info',
+            html: `<p class="paragraf2">${paragraph}</p>`,
+            confirmButtonColor: '#3C7AC0',
+            confirmButtonText: 'Tutup',
+            scrollbarPadding: false
+        });
+    });
+});
